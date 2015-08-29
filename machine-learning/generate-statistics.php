03786a786ceb61101a870eb993dca47ff10615eb
xkong94 <x.l.k@live.com>
2015-08-29 18:53:58 -0400

    updated statistics script testing

--- machine-learning/generate-statistics.php
+++ machine-learning/generate-statistics.php
@@ -7,10 +7,24 @@
     $new_matches = $db->{"RANKED_SOLO.5.14.NA"};
     // Global Item Usage
     $old_item = array();
+    $old_champion_item = array();
     $match_cursor = $old_matches->find();
     foreach ($match_cursor as $match) {
         foreach($match["participants"] as $player) {
-            echo $player["timeline"]["role"] . "\n";
+            // Locate mid laner
+            if ($player["timeline"]["role"] == "SOLO" && $player["timeline"]["role"] == "MIDDLE") {
+                $champion_id = $player["championId"];
+                // Loop through each item
+                for ($i = 0; $i < 6; $i++) {
+                    $item_id = $player["stats"]["item" . $i];
+                    if ($item_id != 0) {
+                        $old_item[$item_id]++;
+                        $old_champion_item[$champion_id][$item_id]++;
+                    }
+                }
+            }
         }
     }
+    print_r($old_item);
+    print_r($old_champion_item);
 ?>
