d82ca9b51df4ca92c3158c9915229e04982ade64
xkong94 <x.l.k@live.com>
2015-08-30 11:49:59 -0400

    test

--- machine-learning/generate-statistics.php
+++ machine-learning/generate-statistics.php
@@ -6,6 +6,7 @@
     $old_matches = $db->{"RANKED_SOLO.5.11.NA"};
     $new_matches = $db->{"RANKED_SOLO.5.14.NA"};
     $global_item = $db->{"ML.STATISTICS.GLOBAL.ITEM"};
+    $global_champion = $db->{"ML.STATISTICS.GLOBAL.CHAMPION"};
     // Conversion Functions
     function GetItem($itemid) {
         global $static_items;
@@ -21,7 +22,9 @@
     $old_item_total = 0; // Mid Laner Count
     $old_champion_item = array(); // ChampionID => [ItemID => Count]
     $old_champion_item_win = array(); // ChampionID => [ItemID => Win]
-    $old_champion_item_total = array(); // ChampionID => Mid Laner Count
+    $old_champion_item_total = array(); // ChampionID => Count
+    $old_champion_win = array(); // ChampionID => Win
+    $old_champion_damage = array(); // ChampionID => Total Damage (Before Divide By Champion Count)
     $match_cursor = $old_matches->find();
     foreach ($match_cursor as $match) {
         // Get winning team
@@ -34,9 +37,20 @@
             // Locate mid laner
             if ($player["timeline"]["role"] == "SOLO" && $player["timeline"]["lane"] == "MIDDLE") {
                 $champion_id = $player["championId"];
+                // Champion Pick Rate
                 if (!isset($old_champion_item_total["$champion_id"]))
                     $old_champion_item_total["$champion_id"] = 0;
                 $old_champion_item_total["$champion_id"]++;
+                // Champion Win Rate
+                if ($player["teamId"] == $winning_team) {
+                    if (!isset($old_champion_win["$champion_id"]))
+                        $old_champion_win["$champion_id"] = 0;
+                    $old_champion_win["$champion_id"]++;
+                }
+                // Champion Damage Dealt
+                if (!isset($old_champion_damage["$champion_id"]))
+                    $old_champion_damage["$champion_id"] = 0;
+                $old_champion_damage["$champion_id"] += $match["stats"]["totalDamageDealtToChampions"];
                 // Loop through each item
                 for ($i = 0; $i < 6; $i++) {
                     $item_id = $player["stats"]["item" . $i];
@@ -72,7 +86,9 @@
     $new_item_total = 0; // Mid Laner Count
     $new_champion_item = array(); // ChampionID => [ItemID => Count]
     $new_champion_item_win = array(); // ChampionID => [ItemID => Win]
-    $new_champion_item_total = array(); // ChampionID => Mid Laner Count
+    $new_champion_item_total = array(); // ChampionID => Count
+    $new_champion_win = array(); // ChampionID => Win
+    $new_champion_damage = array(); // ChampionID => Total Damage (Before Divide By Champion Count)
     $match_cursor = $new_matches->find();
     foreach ($match_cursor as $match) {
         // Get winning team
@@ -85,9 +101,20 @@
             // Locate mid laner
             if ($player["timeline"]["role"] == "SOLO" && $player["timeline"]["lane"] == "MIDDLE") {
                 $champion_id = $player["championId"];
+                // Champion Pick Rate
                 if (!isset($new_champion_item_total["$champion_id"]))
                     $new_champion_item_total["$champion_id"] = 0;
                 $new_champion_item_total["$champion_id"]++;
+                // Champion Win Rate
+                if ($player["teamId"] == $winning_team) {
+                    if (!isset($new_champion_win["$champion_id"]))
+                        $new_champion_win["$champion_id"] = 0;
+                    $new_champion_win["$champion_id"]++;
+                }
+                // Champion Damage Dealt
+                if (!isset($new_champion_damage["$champion_id"]))
+                    $new_champion_damage["$champion_id"] = 0;
+                $new_champion_damage["$champion_id"] += $match["stats"]["totalDamageDealtToChampions"];
                 // Loop through each item
                 for ($i = 0; $i < 6; $i++) {
                     $item_id = $player["stats"]["item" . $i];
@@ -131,6 +158,20 @@
         $document["old_winrate"] = $old_item_win[$itemid] / $itemcount * 100;
         $document["new_winrate"] = $new_item_win[$itemid] / $new_item[$itemid] * 100;
         $global_item->insert($document);
+    }
+    foreach ($old_champion_item as $championid => $championitemdata) {
+        $champion_info = GetChampion($championid);
+        $document = array();
+        $document["id"] = $championid;
+        $document["name"] = $champion_info["name"];
+        $document["image"] = "http://ddragon.leagueoflegends.com/cdn/img/champion/splash/" . $champion_info["name"] . "_0.jpg";
+        $document["old_winrate"] = $old_champion_win[$championid] / $old_champion_item_total[$championid] * 100;
+        $document["new_winrate"] = $new_champion_win[$championid] / $new_champion_item_total[$championid] * 100;
+        $document["old_pickrate"] = $old_champion_item_total[$championid] / $old_item_total;
+        $document["new_pickrate"] = $new_champion_item_total[$championid] / $new_item_total;
+        $document["old_damage"] = $old_champion_damage[$championid] / $old_champion_item_total[$championid];
+        $document["new_damage"] = $new_champion_damage[$championid] / $new_champion_item_total[$championid];
+        $global_champion->insert($document);
     }
     echo "Total Count: $old_item_total\n";
     print_r($old_item);
