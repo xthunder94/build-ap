46dabe11862551505f32aa529424c07a13f47aaa
xkong94 <x.l.k@live.com>
2015-08-30 10:12:38 -0400

    test

--- machine-learning/generate-statistics.php
+++ machine-learning/generate-statistics.php
@@ -14,26 +14,33 @@
         global $static_champions;
         return $static_champions->findOne(array('id' => $championid));
     }
-    print_r(GetItem(3068));
-    exit;
     // Global Item Usage
-    $old_item = array();
-    $old_item_total = 0;
-    $old_champion_item = array();
-    $old_champion_total = array();
+    $old_item = array(); // ItemID => Count
+    $old_item_win = array(); // ItemID => Win
+    $old_item_total = 0; // Mid Laner Count
+    $old_champion_item = array(); // ChampionID => [ItemID => Count]
+    $old_champion_item_win = array(); // ChampionID => [ItemID => Win]
+    $old_champion_item_total = array(); // ChampionID => Mid Laner Count
     $match_cursor = $old_matches->find();
     foreach ($match_cursor as $match) {
+        // Get winning team
+        $winning_team = 0;
+        foreach ($match["teams"] as $team)
+            if ($team["winner"])
+                $winning_team = $team["teamId"];
+        // Loop through each player
         foreach ($match["participants"] as $player) {
             // Locate mid laner
             if ($player["timeline"]["role"] == "SOLO" && $player["timeline"]["lane"] == "MIDDLE") {
                 $champion_id = $player["championId"];
-                if (!isset($old_champion_total["$champion_id"]))
-                    $old_champion_total["$champion_id"] = 0;
-                $old_champion_total["$champion_id"]++;
+                if (!isset($old_champion_item_total["$champion_id"]))
+                    $old_champion_item_total["$champion_id"] = 0;
+                $old_champion_item_total["$champion_id"]++;
                 // Loop through each item
                 for ($i = 0; $i < 6; $i++) {
                     $item_id = $player["stats"]["item" . $i];
                     if ($item_id != 0) {
+                        // Count the item as being used
                         if (!isset($old_item["$item_id"]))
                             $old_item["$item_id"] = 0;
                         $old_item["$item_id"]++;
@@ -42,14 +49,28 @@
                         if (!isset($old_champion_item["$champion_id"]["$item_id"]))
                             $old_champion_item["$champion_id"]["$item_id"] = 0;
                         $old_champion_item["$champion_id"]["$item_id"]++;
+                        // Check if the item is a winning item
+                        if ($player["teamId"] == $winning_team) {
+                            if (!isset($old_item_win["$item_id"]))
+                                $old_item_win["$item_id"] = 0;
+                            $old_item_win["$item_id"]++;
+                            if (!isset($old_champion_item_win["$champion_id"]))
+                                $old_champion_item_win["$champion_id"] = array();
+                            if (!isset($old_champion_item_win["$champion_id"]["$item_id"]))
+                                $old_champion_item_win["$champion_id"]["$item_id"] = 0;
+                            $old_champion_item_win["$champion_id"]["$item_id"]++;
+                        }
                     }
                 }
+                $old_item_total++;
             }
         }
-        $old_item_total++;
     }
+    echo "Total Count: $old_item_total\n";
     print_r($old_item);
+    print_r($old_item_win);
+    echo "==========================================\n";
     print_r($old_champion_item);
-    echo "$old_item_total\n";
-    print_r($old_champion_total);
+    print_r($old_champion_item_win);
+    print_r($old_champion_item_total);
 ?>
