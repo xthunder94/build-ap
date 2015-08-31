821b6dc89593a23f2dddb89499e89c190f5ebd27
xthunder94 <x.l.k@live.com>
2015-08-30 21:28:43 -0400

    changed sort for champion page as well

--- website/js/champion.js
+++ website/js/champion.js
@@ -1,8 +1,49 @@
+var items_collection;
+
 function getParameterByName(name) {
     name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
     var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
         results = regex.exec(location.search);
     return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+}
+
+function populateItem(arr_collection) {
+    $('#overallitemusage').html("");
+    var arrayLength = arr_collection.length;
+    for (var i = 0; i < arrayLength; i++) {
+        if(arr_collection[i].name != null) {
+            var old_usage = arr_collection[i].old_usage > arr_collection[i].new_usage ? 'text-success' : 'text-danger';
+            var new_usage = arr_collection[i].new_usage > arr_collection[i].old_usage ? 'text-success' : 'text-danger';
+            var old_winrate = arr_collection[i].old_winrate > arr_collection[i].new_winrate ? 'text-success' : 'text-danger';
+            var new_winrate = arr_collection[i].new_winrate > arr_collection[i].old_winrate ? 'text-success' : 'text-danger';
+            $('#overallitemusage').append('<tr><td><img class="icon" src="' + arr_collection[i].image + '"></img> ' + arr_collection[i].name + '</td><td class='+old_usage+'>' + arr_collection[i].old_usage + '%</td><td class='+new_usage+'>' + arr_collection[i].new_usage + '%</td><td class='+old_winrate+'>' + arr_collection[i].old_winrate + '%</td><td class='+new_winrate+'>' + arr_collection[i].new_winrate + '%</td></tr>');
+        }
+    }
+}
+
+function i_name() {
+    items_collection.sort(function(a, b) {return a.name.localeCompare(b.name)});
+    populateItem(items_collection);
+}
+
+function i_old_usage() {
+    items_collection.sort(function(a, b) {return b.old_usage-a.old_usage});
+    populateItem(items_collection);
+}
+
+function i_new_usage() {
+    items_collection.sort(function(a, b) {return b.new_usage-a.new_usage});
+    populateItem(items_collection);
+}
+
+function i_old_winrate() {
+    items_collection.sort(function(a, b) {return b.old_winrate-a.old_winrate});
+    populateItem(items_collection);
+}
+
+function i_new_winrate() {
+    items_collection.sort(function(a, b) {return b.new_winrate-a.new_winrate});
+    populateItem(items_collection);
 }
 
 $(document).ready(function() {
@@ -38,17 +79,8 @@ $(document).ready(function() {
         }
     });
     $.getJSON("php/getchampion.php?do=" + encodeURI(champname), function(data) {
-        var arrayLength = data.length;
+        items_collection = data;
         data.sort(function(a, b) {return b.new_usage-a.new_usage});
-        $('#overallitemusage').html("");
-        for (var i = 0; i < arrayLength; i++) {
-            if(data[i].name != null) {
-                var old_usage = data[i].old_usage > data[i].new_usage ? 'text-success' : 'text-danger';
-                var new_usage = data[i].new_usage > data[i].old_usage ? 'text-success' : 'text-danger';
-                var old_winrate = data[i].old_winrate > data[i].new_winrate ? 'text-success' : 'text-danger';
-                var new_winrate = data[i].new_winrate > data[i].old_winrate ? 'text-success' : 'text-danger';
-                $('#overallitemusage').append('<tr><td><img class="icon" src="' + data[i].image + '"></img> ' + data[i].name + '</td><td class='+old_usage+'>' + data[i].old_usage + '%</td><td class='+new_usage+'>' + data[i].new_usage + '%</td><td class='+old_winrate+'>' + data[i].old_winrate + '%</td><td class='+new_winrate+'>' + data[i].new_winrate + '%</td></tr>');
-            }
-        }
+        populateItem(data);
     });
 });
