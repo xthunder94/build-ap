880ee689bcfa22958ae8ca0e4a7654a95b49a728
xthunder94 <x.l.k@live.com>
2015-08-30 21:24:59 -0400

    added it to champions ;)

--- website/js/index.js
+++ website/js/index.js
@@ -15,6 +15,22 @@ function populateItem(arr_collection) {
     }
 }
 
+function populateChampion(arr_collection) {
+    $('#overallchampionusage').html("");
+    var arrayLength = arr_collection.length;
+    for (var i = 0; i < arrayLength; i++) {
+        if(arr_collection[i].name != null) {
+            var old_winrate = arr_collection[i].old_winrate > arr_collection[i].new_winrate ? 'text-success' : 'text-danger';
+            var new_winrate = arr_collection[i].new_winrate > arr_collection[i].old_winrate ? 'text-success' : 'text-danger';
+            var old_pickrate = arr_collection[i].old_pickrate > arr_collection[i].new_pickrate ? 'text-success' : 'text-danger';
+            var new_pickrate = arr_collection[i].new_pickrate > arr_collection[i].old_pickrate ? 'text-success' : 'text-danger';
+            var old_damage = arr_collection[i].old_damage > arr_collection[i].new_damage ? 'text-success' : 'text-danger';
+            var new_damage = arr_collection[i].new_damage > arr_collection[i].old_damage ? 'text-success' : 'text-danger';
+            $('#overallchampionusage').append('<tr><td><img class="icon" src="' + arr_collection[i].icon + '"></img> ' + arr_collection[i].name + '</td><td class='+old_winrate+'>' + arr_collection[i].old_winrate + '%</td><td class='+new_winrate+'>' + arr_collection[i].new_winrate + '%</td><td class='+old_pickrate+'>' + arr_collection[i].old_pickrate + '%</td><td class='+new_pickrate+'>' + arr_collection[i].new_pickrate + '%</td><td class='+old_damage+'>' + arr_collection[i].old_damage + '</td><td class='+new_damage+'>' + arr_collection[i].new_damage + '</td></tr>');
+        }
+    }
+}
+
 function i_name() {
     items_collection.sort(function(a, b) {return a.name.localeCompare(b.name)});
     populateItem(items_collection);
@@ -40,6 +56,41 @@ function i_new_winrate() {
     populateItem(items_collection);
 }
 
+function c_name() {
+    champions_collection.sort(function(a, b) {return a.name.localeCompare(b.name)});
+    populateChampion(champions_collection);
+}
+
+function c_old_winrate() {
+    champions_collection.sort(function(a, b) {return b.old_winrate-a.old_winrate});
+    populateChampion(champions_collection);
+}
+
+function c_new_winrate() {
+    champions_collection.sort(function(a, b) {return b.new_winrate-a.new_winrate});
+    populateChampion(champions_collection);
+}
+
+function c_old_pickrate() {
+    champions_collection.sort(function(a, b) {return b.old_pickrate-a.old_pickrate});
+    populateChampion(champions_collection);
+}
+
+function c_new_pickrate() {
+    champions_collection.sort(function(a, b) {return b.new_pickrate-a.new_pickrate});
+    populateChampion(champions_collection);
+}
+
+function c_old_damage() {
+    champions_collection.sort(function(a, b) {return b.old_damage-a.old_damage});
+    populateChampion(champions_collection);
+}
+
+function c_new_damage() {
+    champions_collection.sort(function(a, b) {return b.new_damage-a.new_damage});
+    populateChampion(champions_collection);
+}
+
 $(document).ready(function() {
     $.getJSON("php/getitems.php", function(data) {
         items_collection = data;
@@ -48,19 +99,7 @@ $(document).ready(function() {
     });
     $.getJSON("php/getchampions.php", function(data) {
         champions_collection = data;
-        var arrayLength = data.length;
         data.sort(function(a, b) {return b.new_pickrate-a.new_pickrate});
-        $('#overallchampionusage').html("");
-        for (var i = 0; i < arrayLength; i++) {
-            if(data[i].name != null) {
-                var old_winrate = data[i].old_winrate > data[i].new_winrate ? 'text-success' : 'text-danger';
-                var new_winrate = data[i].new_winrate > data[i].old_winrate ? 'text-success' : 'text-danger';
-                var old_pickrate = data[i].old_pickrate > data[i].new_pickrate ? 'text-success' : 'text-danger';
-                var new_pickrate = data[i].new_pickrate > data[i].old_pickrate ? 'text-success' : 'text-danger';
-                var old_damage = data[i].old_damage > data[i].new_damage ? 'text-success' : 'text-danger';
-                var new_damage = data[i].new_damage > data[i].old_damage ? 'text-success' : 'text-danger';
-                $('#overallchampionusage').append('<tr><td><img class="icon" src="' + data[i].icon + '"></img> ' + data[i].name + '</td><td class='+old_winrate+'>' + data[i].old_winrate + '%</td><td class='+new_winrate+'>' + data[i].new_winrate + '%</td><td class='+old_pickrate+'>' + data[i].old_pickrate + '%</td><td class='+new_pickrate+'>' + data[i].new_pickrate + '%</td><td class='+old_damage+'>' + data[i].old_damage + '</td><td class='+new_damage+'>' + data[i].new_damage + '</td></tr>');
-            }
-        }
+        populateChampion(data);
     });
 });
